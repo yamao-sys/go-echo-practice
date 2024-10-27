5768307d24cf5f16e0c4daaea9b8c749f4e688b9
yamao-sys <yamaopanku@gmail.com>
2024-10-27 10:09:11 +0900

    認証のエンドポイントの実装

--- app/main.go
+++ app/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"app/controllers"
+	"app/db"
+	"app/routers"
+	"app/services"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("test")
+	dbCon := db.Init()
+
+	// service
+	authService := services.NewAuthService(dbCon)
+
+	// controller
+	authController := controllers.NewAuthController(authService)
+
+	// router
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	authRouter := routers.NewAuthRouter(authController)
+	authRouter.SetRouting(e)
+
+	e.Logger.Fatal(e.Start(":" + os.Getenv("SERVER_PORT")))
 }
